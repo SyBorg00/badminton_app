a5dad49159ecf7364fbb92d0560755082fa24cfc
Sylvann <agawinsylvannjules@gmail.com>
2025-11-02 12:04:58 +0800

    added game view widget (empty)

---
+++ lib/game_view.dart
@@ -0,0 +1,12 @@
+import 'package:badminton_app/model/games.dart';
+import 'package:flutter/material.dart';
+
+class GameView extends StatelessWidget {
+  final Games games;
+  const GameView({super.key, required this.games});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center();
+  }
+}
